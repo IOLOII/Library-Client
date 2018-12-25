a869a444fc7a9c05d43e75927dfe505859c3d4a3
viphuyy <34856171+viphuyy@users.noreply.github.com>
2018-12-25 13:35:15 +0800

    2018/12/25

--- pages/class/class.js
+++ pages/class/class.js
@@ -1,66 +1,101 @@
 // pages/class/class.js
+var app = getApp();
+var mtabW;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    class_title: [1, 2, 3, 4, 5, 6],
+    activeIndex: 0,
+    slideOffset: 0,
+  },
+  tabClick: function(e) {
+    var that = this;
+    var idIndex = e.currentTarget.id;
+    var offsetW = e.currentTarget.offsetLeft; //2种方法获取距离文档左边有多少距离
+    this.setData({
+      activeIndex: idIndex,
+      slideOffset: offsetW
+    });
+    console.log(e);
+    console.log(e._relatedInfo.anchorTargetText)
+  },
+  bindChange: function(e) { //swicper 的绑定事件
+    var current = e.detail.current;
+    if ((current + 1) % 4 == 0) {
+      console.log("fi")
+    }
+    var offsetW = current * mtabW; //2种方法获取距离文档左边有多少距离
+    this.setData({
+      activeIndex: current,
+      slideOffset: offsetW
+    });
 
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    this.data.class_titlel;
+    var that = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        mtabW = res.windowWidth / 4; //设置tab的宽度
+        that.setData({
+          tabW: mtabW
+        })
+      }
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,12 +2,42 @@ Page({
 
   data: {
     index_search_value: '',
-    place_value:'请输入需要查寻的书籍',
+    place_value: '请输入需要查寻的书籍',
     imgsrc: [],
     interval: "1500",
     windowWidth: '',
     windowHeight: '',
-    classItem: ["新东方英语", "新语听书", "微阅读", "考试学习资源库", "QQ阅读", "博看微书屋", "信息视窗", "中邮期刊", "阅读推广平台", "更多"],
+    classItem: [{
+      class: '新东方英语',
+      img_src: '/images/icon_9/xdf.jpg'
+    }, {
+      class: '新语听书',
+        img_src: '/images/icon_9/xyts.jpg'
+    }, {
+      class: '微阅读',
+        img_src: '/images/icon_9/wyd.jpg'
+    }, {
+      class: '考试学习资源库',
+        img_src: '/images/icon_9/ksxxzyk.jpg'
+    }, {
+      class: 'QQ阅读',
+        img_src: '/images/icon_9/qqyd.jpg'
+    }, {
+      class: '博看微书屋',
+        img_src: '/images/icon_9/bkwsw.jpg'
+    }, {
+      class: '信息视窗',
+        img_src: '/images/icon_9/xxsc.jpg'
+    }, {
+    //   class: '中邮期刊',
+    //     img_src: '/images/icon_9/zyqk.jpg'
+    // }, {
+    //   class: '阅读推广平台',
+    //     img_src: '/images/icon_9/ydtgpt.jpg'
+    // }, {
+      class: '更多',
+      img_src: ''
+    }]
   },
   bindInput: function(e) {
     //console.log(e.detail.value) //√
@@ -19,14 +49,14 @@ Page({
   onLoad: function(options) {
     this.getData();
   },
-  getData: function () {
+  getData: function() {
     var that = this;
     wx.request({
       url: 'https://www.easy-mock.com/mock/5bcf443e5d383279bf01b332/example/mock',
       data: {},
       method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
       // header: {}, // 设置请求的 header
-      success: function (res) {
+      success: function(res) {
         console.log(res.data.data.titleimg1)
         // console.log(res.data.data.projects[0].object)
         const imgsrc = res.data.data.titleimg1;
@@ -35,22 +65,22 @@ Page({
 
         })
       },
-      fail: function () {
+      fail: function() {
         // fail
       },
-      complete: function () {
+      complete: function() {
         // complete
       }
     })
   },
-  navigator: function (e) {
+  navigator: function(e) {
     var that = this;
     console.log(e.currentTarget.dataset.index)
     var value = e.currentTarget.dataset.index
     console.log(value);
     wx.navigateTo({
       //url: 'link?value=' + value + ''
-     url: 'nav/10_link/link?value=' + value + ''
+      url: 'nav/10_link/link?value=' + value + ''
     })
   },
 })
